450fa1ac73c3700c7c929fb901823e8bb65408f1
Samuel-LY-Wang <samuellywang@gmail.com>
2025-11-06 13:25:24 -0500

    Added some Java code

---
+++ 160/Java_test/test_java.java
@@ -0,0 +1,5 @@
+public static void main(String[] args) {
+    Person person = new Person("Sam", 18);
+    System.out.println("Name: " + person.getName());
+    System.out.println("Age: " + person.getAge());
+}
